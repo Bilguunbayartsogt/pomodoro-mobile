ad2b341f3359fbee0f2cfef4edafd984d4685545
Bilguun <bilgunbayartsogt@gmail.com>
2024-04-13 15:26:16 +0800

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TextInput, ScrollView } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [petName, setPetName] = useState("");
+	const [petDob, setPetDob] = useState("");
+	const [breed, setBreed] = useState("");
+	const [favToy, setFavToy] = useState("");
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				backgroundColor: "#ecf0f1",
+				marginTop: 50,
+			}}
+		>
+			<ScrollView>
+				<InputField state={email} callback={setEmail} data="email"></InputField>
+				<InputField
+					state={password}
+					callback={setPassword}
+					data="password"
+				></InputField>
+				<InputField
+					state={petName}
+					callback={setPetName}
+					data="pet name"
+				></InputField>
+				<InputField
+					state={petDob}
+					callback={setPetDob}
+					data="pet date of birth"
+				></InputField>
+				<InputField state={breed} callback={setBreed} data="breed"></InputField>
+				<InputField
+					state={favToy}
+					callback={setFavToy}
+					data="favorite toy"
+				></InputField>
+			</ScrollView>
+		</View>
+	);
+};
+
+export default App;
+
+export const InputField = (props) => {
+	return (
+		<View
+			style={{
+				padding: 16,
+			}}
+		>
+			<Text
+				style={{
+					padding: 8,
+					fontSize: 18,
+				}}
+			>
+				Enter your {props.data}: {props.data !== "password" ? props.state : ""}
+			</Text>
+			<TextInput
+				secureTextEntry={props.data === "password"}
+				placeholder="type here..."
+				onChangeText={(value) => {
+					props.callback(value);
+				}}
+				style={{
+					padding: 8,
+					fontSize: 18,
+					borderWidth: 1,
+					borderColor: "black",
+				}}
+			/>
+		</View>
+	);
+};
