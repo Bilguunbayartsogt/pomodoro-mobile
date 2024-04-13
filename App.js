8e5d10dee11c003ef4af7ac38f873f0ff2eb23d1
Bilguun <bilgunbayartsogt@gmail.com>
2024-04-13 15:54:30 +0800

    makes no sense at the moment. will improve

--- App.js
+++ App.js
@@ -1,81 +1,67 @@
-import React, { useState } from "react";
-import { View, Text, TextInput, ScrollView } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, TextInput, ScrollView, Button } from "react-native";
+import { formatTime } from "./utils/functions";
 
-const App = () => {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [petName, setPetName] = useState("");
-	const [petDob, setPetDob] = useState("");
-	const [breed, setBreed] = useState("");
-	const [favToy, setFavToy] = useState("");
+const Pomodoro = () => {
+	const DEF_SESS = 1500;
+	const DEF_BRK = 300;
+
+	const [sessionTimer, setSessionTimer] = useState(DEF_SESS);
+	const [breakTimer, setBreakTimer] = useState(DEF_BRK);
+
+	const [sessionLength, setSessionLength] = useState(DEF_SESS);
+	const [breakLength, setBreakLength] = useState(DEF_BRK);
+
+	const [sessionInterval, setSessionInterval] = useState(null);
+	const [breakInterval, setBreakInterval] = useState(null);
+
+	const [isSession, setIsSession] = useState(true);
+	const [isUpdateVisible, setIsUpdateVisible] = useState(true);
 
 	return (
 		<View
 			style={{
 				flex: 1,
 				justifyContent: "center",
+				alignItems: "center",
 				backgroundColor: "#ecf0f1",
 				marginTop: 50,
 			}}
 		>
-			<ScrollView>
-				<InputField state={email} callback={setEmail} data="email"></InputField>
-				<InputField
-					state={password}
-					callback={setPassword}
-					data="password"
-				></InputField>
-				<InputField
-					state={petName}
-					callback={setPetName}
-					data="pet name"
-				></InputField>
-				<InputField
-					state={petDob}
-					callback={setPetDob}
-					data="pet date of birth"
-				></InputField>
-				<InputField state={breed} callback={setBreed} data="breed"></InputField>
-				<InputField
-					state={favToy}
-					callback={setFavToy}
-					data="favorite toy"
-				></InputField>
-			</ScrollView>
-		</View>
-	);
-};
-
-export default App;
-
-export const InputField = (props) => {
-	return (
-		<View
-			style={{
-				padding: 16,
-			}}
-		>
 			<Text
 				style={{
-					padding: 8,
-					fontSize: 18,
+					fontSize: 40,
 				}}
 			>
-				Enter your {props.data}: {props.data !== "password" ? props.state : ""}
+				Pomodoro
 			</Text>
-			<TextInput
-				secureTextEntry={props.data === "password"}
-				placeholder="type here..."
-				onChangeText={(value) => {
-					props.callback(value);
-				}}
+			<View>
+				<Text
+					style={{
+						fontSize: 30,
+					}}
+				>
+					Status: {"\n"}
+					<Text>{formatTime(sessionTimer)}</Text>
+				</Text>
+			</View>
+			<View
 				style={{
-					padding: 8,
-					fontSize: 18,
-					borderWidth: 1,
-					borderColor: "black",
+					flex: 1,
+					flexDirection: "row",
 				}}
-			/>
+			>
+				<Text>
+					Session length: {"\n"}
+					<Text>{sessionLength / 60}</Text>
+				</Text>
+				<Text>
+					Break length: {"\n"}
+					<Text>{breakLength / 60}</Text>
+				</Text>
+			</View>
 		</View>
 	);
 };
+
+export default Pomodoro;
